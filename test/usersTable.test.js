aeaff8d2b6825584d771b60f9ccfeda203713f71
Michael Murray <michaelmurray6298@gmail.com>
2017-06-23 12:17:53 -0700

    adds users migration test

---
+++ test/usersTable.test.js
@@ -0,0 +1,62 @@
+process.env.NODE_ENV = 'test';
+
+const assert = require('chai').assert;
+const { suite, test } = require('mocha');
+const knex = require('../knex');
+const { addDatabaseHooks } = require('./utils')
+suite('users migration', addDatabaseHooks(() => {
+  test('users table', (done) => {
+    knex('users').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'users_id_seq\'::regclass)'
+          },
+
+          first_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          last_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          email: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          hashed_password: {
+            type: 'text',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'\'::text'
+          }
+				};
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+}));
