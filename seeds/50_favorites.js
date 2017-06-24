da903d2f48d0a14fd3264d95188b3d84580eab62
Johan Brattemark <johan.brattemark@gmail.com>
2017-06-23 18:06:02 -0700

    Add seed test file and seed file itself for 'favorites'

--- seeds/50_favorites.js
+++ seeds/50_favorites.js
@@ -1,13 +1,17 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  // return knex('table_name').del()
-  //   .then(function () {
-  //     // Inserts seed entries
-  //     return knex('table_name').insert([
-  //       {id: 1, colName: 'rowValue1'},
-  //       {id: 2, colName: 'rowValue2'},
-  //       {id: 3, colName: 'rowValue3'}
-  //     ]);
-  //   });
+  return knex('favorites')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('favorites').insert([
+        { id: 1, user_id: 2, trail_id: 1, ranking: 2 },
+        { id: 2, user_id: 1, trail_id: 2, ranking: 5 },
+        { id: 3, user_id: 2, trail_id: 1, ranking: 2 }
+      ]);
+    })
+    .then(() =>
+      knex.raw(
+        "SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites))"
+      )
+    );
 };
