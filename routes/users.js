e7cb391857a74b098b8ec77271ab538e8996b31a
Johan Brattemark <johan.brattemark@gmail.com>
2017-06-26 12:40:27 -0700

    Add route for GET /users"

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const Users = require('../models/Users');
+
+router.get('/users', (req, res) => {
+  let users = new Users();
+  let promise = users.getUsers();
+
+  promise
+    .then(users => {
+      if (!users) {
+        res.status(404).send('No users found');
+      }
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+
+// router.get('/users/:id', (req, res) => {
+//   let users = new Users();
+//   let promise = users.getUser(req.params.id);
+//
+//   promise
+//   .then(user => {
+//     if (!user) {
+//       res.status(404).send('Not found');
+//     }
+//     res.json(user);
+//   })
+//   .catch(err => {
+//     res.status(500).send(err);
+//   });
+// });
+
+module.exports = router;
