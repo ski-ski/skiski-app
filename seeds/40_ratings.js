3cf6cb9dcffa2fdfe1d828c0ebb806b82609c75b
Michael Murray <michaelmurray6298@gmail.com>
2017-06-26 10:06:21 -0700

    Add tested ratings seed file

--- seeds/40_ratings.js
+++ seeds/40_ratings.js
@@ -1,13 +1,31 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  // return knex('table_name').del()
-  //   .then(function () {
-  //     // Inserts seed entries
-  //     return knex('table_name').insert([
-  //       {id: 1, colName: 'rowValue1'},
-  //       {id: 2, colName: 'rowValue2'},
-  //       {id: 3, colName: 'rowValue3'}
-  //     ]);
-  //   });
+  return knex('ratings').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('ratings').insert([
+        {
+          id: 1,
+          user_id: 1,
+          trail_id: 1,
+          rating: 4,
+					review:"very good",
+					created_at: new Date('2016-06-29 14:26:16 UTC'),
+          updated_at: new Date('2016-06-29 14:26:16 UTC')
+        },
+				{
+          id: 2,
+          user_id: 2,
+          trail_id: 2,
+          rating: 2,
+					review:"ok",
+					created_at: new Date('2016-06-29 14:26:16 UTC'),
+          updated_at: new Date('2016-06-29 14:26:16 UTC')
+        }
+
+      ]);
+    })
+    .then(function(){
+          return knex.raw(`SELECT setval('ratings_id_seq', (SELECT MAX(id) FROM ratings))`)
+      });
 };
