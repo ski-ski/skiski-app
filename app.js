344ffdd6567d88f9a58cabbca167260fd48d33fb
Johan Brattemark <johan.brattemark@gmail.com>
2017-06-26 12:40:27 -0700

    Add route for GET /users"

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const portNumber = 3000;
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
+const users = require('./routes/users');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(users);
+
 app.use((req, res) => {
   res.sendStatus(404);
 });
