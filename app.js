91ebb17135b286aca70256cac4e22f1790683867
Michael Murray <michaelmurray6298@gmail.com>
2017-06-27 14:06:00 -0700

    Add resorts routes

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const users = require('./routes/users');
 const token = require('./routes/token');
-
+const resorts = require('./routes/resorts');
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(users);
-app.use(token)
+app.use(token);
+app.use(resorts);
 
 app.use((req, res) => {
   res.sendStatus(404);
