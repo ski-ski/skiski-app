72c242df2dca5a93952749d9e554987778119be0
Michael Murray <michaelmurray6298@gmail.com>
2017-06-29 01:33:49 -0700

    Add apidocs for ratings, favorites, and resorts

--- routes/ratings.js
+++ routes/ratings.js
@@ -1,14 +1,85 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const humps = require('humps');
-const _ = require('lodash');
-const Ratings = require('../repositories/Ratings');
+const humps = require("humps");
+const _ = require("lodash");
+const Ratings = require("../repositories/Ratings");
 // const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
 
+/**
+ * @apiDefine NotFoundError
+ *
+ * @apiError NotFoundError The requested path was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *
+ *       "Not found"
+ *
+ */
 
-// Create one
-router.post('/ratings', (req, res, next) => {
+/**
+  * @apiDefine UnAuthorized
+  *
+  * @apiError UnAuthorized The user is not authorized to access this route.
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 401
+  *
+  *    "Not authorized"
+  *
+*/
+
+/**
+ * @apiDefine ServerError
+ *
+ * @apiError ServerError Interal server error.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 505 Interal Server Error
+ *
+ *     "{}"
+ *
+ */
+
+/**
+  * @api {post} /ratings Create a rating for a trail
+  * @apiVersion 1.0.0
+  * @apiName PostRating
+  * @apiGroup Ratings
+  *
+  * @apiDescription Users can create a rating for a trail.
+  *
+  * @apiParam {Number} userId    User ID.
+  * @apiParam {Number} trailId   Trail ID.
+  * @apiParam {Number} rating   Trail Rating.
+  * @apiParam {String} review   Trail review.
+  *
+  * @apiExample Example usage:
+  * curl -d 'userId=1&trailId=1&rating=4&review="very good"' http://localhost/favorites
+  *
+  * @apiSuccess {Number} id           Rating ID.
+  * @apiSuccess {Number} userId       User ID.
+  * @apiSuccess {Number} trailId      Trail ID.
+  * @apiSuccess {Number} rating       Trail Rating.
+  * @apiSuccess {String} review       Tail review.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  *  {
+  *     "id": 1,
+  *     "user_id": 1,
+  *     "trail_id": 1,
+  *     "rating": 4,
+  *     "review": "very good"
+  *   }
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
+
+router.post("/ratings", (req, res, next) => {
   let ratings = new Ratings();
   let ratingData = {
     user_id: req.body.userId,
@@ -16,80 +87,237 @@ router.post('/ratings', (req, res, next) => {
     rating: req.body.rating,
     review: req.body.review
   };
-  ratings.createRating(ratingData)
-  .then((rating) => {
-    res.send(humps.camelizeKeys(rating[0]));
-  })
-  .catch((err) => {
-    next(err);
-  });
+  ratings
+    .createRating(ratingData)
+    .then(rating => {
+      res.send(humps.camelizeKeys(rating[0]));
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
-
-// Read one
-router.get('/ratings/:id', (req, res) => {
+/**
+  * @api {get} /ratings/:id View a rating
+  * @apiVersion 1.0.0
+  * @apiName ViewRating
+  * @apiGroup Ratings
+  *
+  * @apiDescription Users can view a rating for a trail.
+  *
+  * @apiParam {Number} id    Rating ID.
+  *
+  * @apiExample Example usage:
+  * curl http://localhost/favorites/1
+  *
+  * @apiSuccess {Number} id           Rating ID.
+  * @apiSuccess {Number} userId       User ID.
+  * @apiSuccess {Number} trailId      Trail ID.
+  * @apiSuccess {Number} rating       Trail Rating.
+  * @apiSuccess {String} review       Tail review.
+  * @apiSuccess {String} createdAt    Timestamp when rating was created.
+  * @apiSuccess {String} updatedAt    Timestamp when rating was updated.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  *  {
+  *     "id": 1,
+  *     "user_id": 1,
+  *     "trail_id": 1,
+  *     "rating": 4,
+  *     "review":"very good",
+  *     "created_at": '2016-06-29 14:26:16 UTC',
+  *     "updated_at": '2016-06-29 14:26:16 UTC'
+  *   }
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
+router.get("/ratings/:id", (req, res) => {
   let ratings = new Ratings();
-  ratings.getRating(req.params.id)
-  .then(rating => {
-    if (!rating) {
-      res.status(404).send('Not found');
-    }
-    res.json(humps.camelizeKeys(rating[0]));
-  })
-  .catch(err => {
-    res.status(500).send(err);
-  });
+  ratings
+    .getRating(req.params.id)
+    .then(rating => {
+      if (!rating) {
+        res.status(404).send("Not found");
+      }
+      res.json(humps.camelizeKeys(rating[0]));
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 });
 
-
-// Update one
-router.post('/ratings/:id', (req, res) =>{
+/**
+  * @api {post} /ratings/:id Update a rating for a trail
+  * @apiVersion 1.0.0
+  * @apiName UpdateRating
+  * @apiGroup Ratings
+  *
+  * @apiDescription Users can update a rating for a trail.
+  *
+  * @apiParam {Number} id        Rating ID.
+  * @apiParam {Number} userId    User ID.
+  * @apiParam {Number} trailId   Trail ID.
+  * @apiParam {Number} rating   Trail Rating.
+  * @apiParam {String} review   Trail review.
+  *
+  * @apiExample Example usage:
+  * curl -d 'review="sucks"' http://localhost/favorites/1
+  *
+  * @apiSuccess {Number} id           Rating ID.
+  * @apiSuccess {Number} userId       User ID.
+  * @apiSuccess {Number} trailId      Trail ID.
+  * @apiSuccess {Number} rating       Trail Rating.
+  * @apiSuccess {String} review       Tail review.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  *  {
+  *     "id": 1,
+  *     "user_id": 1,
+  *     "trail_id": 1,
+  *     "rating": 4,
+  *     "review": "very good"
+  *   }
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
+router.post("/ratings/:id", (req, res) => {
   let ratings = new Ratings();
-  let {user_id, trail_id, rating, review} = humps.decamelizeKeys(req.body);
-  let validFields = {user_id, trail_id, rating, review};
-  let filteredObject =  _(validFields).omitBy(_.isUndefined).omitBy(_.isNull).value();
-  ratings.updateRating(req.params.id, filteredObject)
-  .then((rating) => {
+  let { user_id, trail_id, rating, review } = humps.decamelizeKeys(req.body);
+  let validFields = { user_id, trail_id, rating, review };
+  let filteredObject = _(validFields)
+    .omitBy(_.isUndefined)
+    .omitBy(_.isNull)
+    .value();
+  ratings.updateRating(req.params.id, filteredObject).then(rating => {
     res.json(humps.camelizeKeys(rating[0]));
   });
 });
 
-
-// Delete one
-router.delete('/ratings/:id', (req, res) => {
+/**
+  * @api {delete} /ratings/:id delete a rating for a trail
+  * @apiVersion 1.0.0
+  * @apiName DeleteRating
+  * @apiGroup Ratings
+  *
+  * @apiDescription Users can delete a rating for a trail.
+  *
+  * @apiParam {Number} id        Rating ID.
+  *
+  * @apiExample Example usage:
+  * curl -X http://localhost/favorites/1
+  *
+  * @apiSuccess {Number} id           Rating ID.
+  * @apiSuccess {Number} userId       User ID.
+  * @apiSuccess {Number} trailId      Trail ID.
+  * @apiSuccess {Number} rating       Trail Rating.
+  * @apiSuccess {String} review       Tail review.
+  * @apiSuccess {String} createdAt    Timestamp when rating was created.
+  * @apiSuccess {String} updatedAt    Timestamp when rating was updated.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  *  {
+  *     "id: 1",
+  *     "user_id": 1,
+  *     "trail_id": 1,
+  *     "rating": 4,
+  *     "review": "sucks",
+  *     "created_at": '2016-06-29 14:26:16 UTC',
+  *     "updated_at": '2016-06-29 14:26:16 UTC'
+  *   }
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
+router.delete("/ratings/:id", (req, res) => {
   let ratings = new Ratings();
   let id = req.params.id;
   if (isNaN(id)) {
     return res.sendStatus(404);
   }
-  ratings.deleteRating(id)
-  .then((rating) => {
-    if (!rating[0]) {
-      res.sendStatus(404);
-    } else {
-      res.send(humps.camelizeKeys(rating[0]));
-    }
-  })
-  .catch(err => {
-    res.status(500).send(err);
-  });
+  ratings
+    .deleteRating(id)
+    .then(rating => {
+      if (!rating[0]) {
+        res.sendStatus(404);
+      } else {
+        res.send(humps.camelizeKeys(rating[0]));
+      }
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 });
 
-
-// Read all
-router.get('/ratings', (req, res) => {
+/**
+  * @api {get} /ratings View all ratings
+  * @apiVersion 1.0.0
+  * @apiName ViewRatings
+  * @apiGroup Ratings
+  *
+  * @apiDescription Users can view all ratings for all trail.
+  *
+  * @apiExample Example usage:
+  * curl http://localhost/favorites
+  *
+  * @apiSuccess {Object[]} ratings Array of objects with favorited trails.
+  *
+  * @apiSuccess {Number} id           Rating ID.
+  * @apiSuccess {Number} userId       User ID.
+  * @apiSuccess {Number} trailId      Trail ID.
+  * @apiSuccess {Number} rating       Trail Rating.
+  * @apiSuccess {String} review       Tail review.
+  * @apiSuccess {String} createdAt    Timestamp when rating was created.
+  * @apiSuccess {String} updatedAt    Timestamp when rating was updated.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  * [
+  *   {
+  *     "id": 1,
+  *     "user_id": 1,
+  *     "trail_id": 1,
+  *     "rating": 4,
+  *     "review":"very good",
+  *     "created_at": '2016-06-29 14:26:16 UTC',
+  *     "updated_at": '2016-06-29 14:26:16 UTC'
+  *   },
+  *  {
+  *     "id": 2,
+  *     "user_id": 1,
+  *     "trail_id": 2,
+  *     "rating": 5,
+  *     "review":"super good",
+  *     "created_at": '2016-06-29 14:26:16 UTC',
+  *     "updated_at": '2016-06-29 14:26:16 UTC'
+  *   }
+  *
+  * ]
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
+router.get("/ratings", (req, res) => {
   let ratings = new Ratings();
-  ratings.getRatings()
-  .then(ratings => {
-    if (!ratings) {
-      res.status(404).send('No ratings found');
-    }
-    res.json(humps.camelizeKeys(ratings));
-  })
-  .catch(err => {
-    res.status(500).send(err);
-  });
+  ratings
+    .getRatings()
+    .then(ratings => {
+      if (!ratings) {
+        res.status(404).send("No ratings found");
+      }
+      res.json(humps.camelizeKeys(ratings));
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 });
-
 
 module.exports = router;
--- routes/resorts.js
+++ routes/resorts.js
@@ -5,7 +5,90 @@ const humps = require("humps");
 const _ = require("lodash");
 const jwt = require("jsonwebtoken");
 const Resorts = require("../repositories/Resorts");
+/**
+ * @apiDefine NotFoundError
+ *
+ * @apiError NotFoundError The requested path was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *
+ *       "Not found"
+ *
+ */
 
+/**
+  * @apiDefine UnAuthorized
+  *
+  * @apiError UnAuthorized The user is not authorized to access this route.
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 401
+  *
+  *    "Not authorized"
+  *
+*/
+
+/**
+ * @apiDefine ServerError
+ *
+ * @apiError ServerError Interal server error.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 505 Interal Server Error
+ *
+ *     "{}"
+ *
+ */
+
+/**
+  * @api {get} /resorts View all resorts
+  * @apiVersion 1.0.0
+  * @apiName ViewResorts
+  * @apiGroup Resorts
+  *
+  * @apiDescription Users can view all resorts.
+  *
+  * @apiExample Example usage:
+  * curl http://localhost/resorts
+  *
+  * @apiSuccess {Object[]} ratings Array of objects with resorts.
+  *
+  * @apiSuccess {Number} id                Resort ID.
+  * @apiSuccess {Number} name              Resort Name.
+  * @apiSuccess {Number} city              Resort City.
+  * @apiSuccess {Number} windspeed         Windspeed at resort.
+  * @apiSuccess {String} highTemperature   highTemperature at resort.
+  * @apiSuccess {String} lowTemperature    lowTemperature at resort.
+  * @apiSuccess {String} snowDepth         snowDepth at resort.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  * [
+  *   {
+  *     id: 1,
+  *     name: 'Squaw Valley',
+  *     city: 'Olympic_Valley',
+  *     windspeed: 0,
+  *     high_temperature:0,
+  *     low_temperature:0,
+  *     snow_depth: 0
+  *   },
+  *   {
+  *     id: 2,
+  *     name: 'Heavenly',
+  *     city: 'South_Lake_Tahoe',
+  *     windspeed: 2,
+  *     high_temperature:55,
+  *     low_temperature:33,
+  *     snow_depth: 0
+  *   }
+  * ]
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
 router.get("/resorts", (req, res) => {
   let resorts = new Resorts();
   let promise = resorts.getResorts();
@@ -21,7 +104,42 @@ router.get("/resorts", (req, res) => {
       res.status(500).send(err);
     });
 });
-
+/**
+  * @api {get} /resorts View a resort
+  * @apiVersion 1.0.0
+  * @apiName ViewResort
+  * @apiGroup Resorts
+  *
+  * @apiDescription Users can view a resort.
+  *
+  * @apiParam {Number} id                 Resort ID
+  * @apiExample Example usage:
+  * curl http://localhost/resorts/:id
+  *
+  * @apiSuccess {Number} id                Resort ID.
+  * @apiSuccess {Number} name              Resort Name.
+  * @apiSuccess {Number} city              Resort City.
+  * @apiSuccess {Number} windspeed         Windspeed at resort.
+  * @apiSuccess {String} highTemperature   highTemperature at resort.
+  * @apiSuccess {String} lowTemperature    lowTemperature at resort.
+  * @apiSuccess {String} snowDepth         snowDepth at resort.
+  *
+  * @apiSuccessExample Success-Response:
+  *
+  *   HTTP/1.1 200 OK
+  *   {
+  *     id: 2,
+  *     name: 'Heavenly',
+  *     city: 'South_Lake_Tahoe',
+  *     windspeed: 2,
+  *     high_temperature:55,
+  *     low_temperature:33,
+  *     snow_depth: 0
+  *   }
+  *
+  * @apiUse ServerError
+  * @apiUse NotFoundError
+ */
 router.get("/resorts/:id", (req, res) => {
   let resorts = new Resorts();
   let promise = resorts.getResort(req.params.id);
